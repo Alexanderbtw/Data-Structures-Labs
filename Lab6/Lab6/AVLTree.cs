0d5015250e481b30073bfaefac6ca1d1b7b52d23
Alexanderbtw <sgoldebaev@gmail.com>
2023-11-14 20:55:26 +0300

    Program update

--- Lab6/Lab6/AVLTree.cs
+++ Lab6/Lab6/AVLTree.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace Lab6
 {
@@ -190,10 +185,7 @@ namespace Lab6
             }
             else
             {
-                if (Head != null)
-                {
-                    Head.Balance();
-                }
+                Head?.Balance();
             }
 
             return true;
--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -9,28 +9,40 @@
             tree.Add(3);
             tree.Add(2);
             tree.Add(4);
+            tree.Add(5);
             tree.Add(12);
             tree.Add(15);
             tree.Add(11);
             tree.Add(25);
 
-            //tree.Remove(11);
+            Console.WriteLine($"Дерево содержит 11: {tree.Contains(11)}");
+            tree.Remove(11);
+            Console.WriteLine($"Дерево содержит 11: {tree.Contains(11)}");
 
+            var node = tree.Find(4, out int steps);
+            Console.WriteLine($"\nЭлемент {node!.Value} присутствует в дереве. Был найден за {steps} шагов");
+
+            Console.WriteLine("\nВывод дерева с помощью итератора:");
             foreach (int i in tree)
             {
                 Console.WriteLine(i);
             }
 
+            Console.WriteLine("\nВывод дерева с помощью ToString:");
             Console.WriteLine(tree.ToString());
 
+            Console.WriteLine("\nВывод дерева с помощью Display:");
             tree.Display();
-            Console.WriteLine(tree.Count);
+            Console.WriteLine($"\nКол-во элементов: {tree.Count}");
 
             tree.LevelOut();
-            Console.WriteLine();
 
+            Console.WriteLine("\nВывод дерева с помощью Display после LevelOut (задания):");
             tree.Display();
-            Console.WriteLine(tree.Count);
+            Console.WriteLine($"\nКол-во элементов: {tree.Count}");
+
+            tree.Clear();
+            Console.WriteLine($"\nДерево после очистки: {tree}");
         }
     }
 }
